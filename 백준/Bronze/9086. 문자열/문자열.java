d553713f739dabefbca69324cd7d5cb1a0966e86
연주원 <50977497+joo1yeon@users.noreply.github.com>
2024-01-22 17:54:14 +0900

    [Bronze V] Title: 문자열, Time: 72 ms, Memory: 11300 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+class Main {
+    public static void main (String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int T = Integer.parseInt(br.readLine());
+        
+        for (int tc = 1; tc <= T; tc++) {
+            String s = br.readLine();
+            sb.append(s.charAt(0)).append(s.charAt(s.length() - 1)).append('\n');
+        }
+        
+        System.out.println(sb);
+    }
+}
