5027a16f6a3ee1369a9cb0d1305e8971fd46f5c9
연주원 <50977497+joo1yeon@users.noreply.github.com>
2024-01-22 17:17:17 +0900

    [Bronze V] Title: 이상한 기호, Time: 80 ms, Memory: 11556 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        
+        long answer = (long)Math.pow(A, 2) - (long)Math.pow(B, 2);
+        
+        System.out.println(answer);
+    }
+}
