0c24414dbed5cfc5d7f60afb1a81cabc8fb96e24
연주원 <50977497+joo1yeon@users.noreply.github.com>
2024-01-22 17:26:44 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 76 ms, Memory: 11476 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+class Main {
+    
+    public static void main (String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String s = br.readLine();
+        
+        System.out.println(s.length());
+    }
+}
